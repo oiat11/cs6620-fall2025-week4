7aab85d016b6082f075d9c59da2f9f2d7ef6f0d8
chavalab <chavala.b@northeastern.edu>
2026-05-26 15:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-import sys
-
-
 def add(a, b):
-    """Add two numbers"""
-    
+    """Add two numbers""" 
     return a + b
   
 
 def subtract(a, b):
     """Subtract two numbers"""
     return a - b
-
+    
 
 def multiply(a, b):
     """Multiply two numbers"""
     return a * b
-
 
 
 def divide(a, b):
